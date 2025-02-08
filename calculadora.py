b053a1bd37c4e5db2d3d92200839c20fb4598e75
Emanuel Gomes <emanuelgomesqa@gmail.com>
2025-02-08 16:32:50 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+import math
+
+class Calculadora:
+    
+    def adicionar(self, a, b):
+        return a + b
+    
+    def subtrair(self, a, b):
+        return a - b
+    
+    def multiplicar(self, a, b):
+        return a * b
+    
+    def dividir(self, a, b):
+        if b == 0:
+            return "Erro: Divisão por zero não permitida!"
+        return a / b
+    
+    def fatorial(self, a):
+        if a < 0:
+            return "Erro: Fatorial de número negativo não existe!"
+        return math.factorial(a)
+    
+    def potencia(self, a, b):
+        return a ** b
